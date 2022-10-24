f2d4ac3c364aeeff56b0bb9d917f326b1f379616
Omar <omar.abdelhameed@scandiweb.com>
2022-10-24 12:11:02 +0200

    Update InsertDummyProductsPatch.php

--- Scandiweb/Test/Setup/Patch/Data/InsertDummyProductsPatch.php
+++ Scandiweb/Test/Setup/Patch/Data/InsertDummyProductsPatch.php
@@ -8,7 +8,7 @@
  * @license http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
  */
 
-namespace Example\Migration\Setup\Patch\Data;
+namespace Scandiweb\Test\Setup\Patch\Data;
 
 use Magento\Catalog\Api\CategoryLinkManagementInterface;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
